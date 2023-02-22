b544954aee84da326453e3a0e41b206ff8d95b21
MF <shiliangfan@gmail.com>
2023-02-21 17:12:39 -0700

    Iam leaving now

--- unm_raystation/development/processing_export.py
+++ unm_raystation/development/processing_export.py
@@ -24,11 +24,11 @@ class DCMExportDestination:
         RetainUIDS: bool = False
         RetainSafePrivateAttributes: bool = False
 
-        def _default_anonymization_settings(self) -> dict:
-            return self.AnonymizationSettings().__dict__
+    def get_anonymization_settings(self) -> dict:
+        return self.AnonymizationSettings().__dict__
 
     def __post_init__(self):
-        anonymization_settings: dict = field(default_factory=self._default_anonymization_settings)
+        self.anonymization_settings: dict = field(default_factory=self.get_anonymization_settings)
         print(self.anonymization_settings)
 
     def handle_log_completion(self, result):
