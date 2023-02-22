a0ac7089766036cd6c490c2fa1332de76d233d39
MF <shiliangfan@gmail.com>
2023-02-22 11:36:49 -0700

    anon settings worked

--- unm_raystation/development/processing_export.py
+++ unm_raystation/development/processing_export.py
@@ -7,29 +7,24 @@ from dataclasses import dataclass, field
 
 
 @dataclass
-class DCMExportDestination:
-    """Class to configure DICOM export destination options"""
-
-    name: str
-
-    @dataclass
-    class AnonymizationSettings:
-        # Anonymization settings
-        anonymize: bool = False
-        AnonymizedName: str = "anonymizedName"
-        AnonymizedID: str = "anonymizedID"
-        RetainDates: bool = False
-        RetainDeviceIdentity: bool = False
-        RetainInstitutionIdentity: bool = False
-        RetainUIDS: bool = False
-        RetainSafePrivateAttributes: bool = False
+class AnonymizationSettings:
+    anonymize: bool = False
+    AnonymizedName: str = "anonymizedName"
+    AnonymizedID: str = "anonymizedID"
+    RetainDates: bool = False
+    RetainDeviceIdentity: bool = False
+    RetainInstitutionIdentity: bool = False
+    RetainUIDS: bool = False
+    RetainSafePrivateAttributes: bool = False
 
     def get_anonymization_settings(self) -> dict:
-        return self.AnonymizationSettings().__dict__
+        return self.__dict__
 
-    def __post_init__(self):
-        self.anonymization_settings: dict = field(default_factory=self.get_anonymization_settings)
-        print(self.anonymization_settings)
+
+@dataclass
+class DCMExportDestination:
+    name: str
+    anonymization_settings: AnonymizationSettings
 
     def handle_log_completion(self, result):
         return
@@ -45,7 +40,8 @@ class DCMExportDestination:
 
 
 def main():
-    test_dicom_destination = DCMExportDestination(name="test_destination")
+    test_anon_settings = AnonymizationSettings()
+    test_dicom_destination = DCMExportDestination("test_destination", test_anon_settings)
 
 
 if __name__ == "__main__":
